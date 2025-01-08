e8b5a18996f147e78abbf8d5dc12a7b818a17667
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2025-01-08 14:51:59 +0530

    Create cherry_creek_trail.sql

---
+++ cherry_creek_trail.sql
@@ -0,0 +1,74 @@
+create secure materialized view MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.SMV_CHERRY_CREEK_TRAIL(
+	POINT_ID,
+	TRAIL_NAME,
+	LNG,
+	LAT,
+	COORD_PAIR,
+    DISTANCE_TO_MELANIES
+) as
+select 
+ value:sequence_1 as point_id,
+ value:trail_name::varchar as trail_name,
+ value:latitude::number(11,8) as lng,
+ value:longitude::number(11,8) as lat,
+ lng||' '||lat as coord_pair,
+ locations.distance_to_mc(lng,lat) as distance_to_melanies
+from t_cherry_creek_trail;
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+  SELECT
+  'DLKW09' as step
+  ,( select row_count
+     from mels_smoothie_challenge_db.information_schema.tables
+     where table_schema = 'TRAILS'
+    and table_name = 'SMV_CHERRY_CREEK_TRAIL')   
+   as actual
+  ,3526 as expected
+  ,'Secure Materialized View Created' as description
+ ); 
+
+
+ CREATE OR REPLACE EXTERNAL VOLUME iceberg_external_volume
+   STORAGE_LOCATIONS =
+      (
+         (
+            NAME = 'iceberg-s3-us-west-2'
+            STORAGE_PROVIDER = 'S3'
+            STORAGE_BASE_URL = 's3://uni-dlkw-iceberg'
+            STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::321463406630:role/dlkw_iceberg_role'
+            STORAGE_AWS_EXTERNAL_ID = 'dlkw_iceberg_id'
+         )
+      );
+
+
+DESC EXTERNAL VOLUME iceberg_external_volume;
+
+create or replace database my_iceberg_db
+ catalog = 'SNOWFLAKE'
+ external_volume = 'iceberg_external_volume';
+
+
+ set table_name = 'CCT_'||current_account();
+ select $table_name;
+
+create iceberg table identifier($table_name) (
+    point_id number(10,0)
+    , trail_name string
+    , coord_pair string
+    , distance_to_melanies decimal(20,10)
+    , user_name string
+)
+  BASE_LOCATION = $table_name
+  AS SELECT top 100
+    point_id
+    , trail_name
+    , coord_pair
+    , distance_to_melanies
+    , current_user()
+  FROM MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.SMV_CHERRY_CREEK_TRAIL;
+
+
+  select $table_name;
+  
