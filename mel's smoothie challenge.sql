cbb922f966944d5028b6c3667ece590ec1c17a89
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2025-01-08 14:50:18 +0530

    Create mel's smoothie challenge.sql

---
+++ mel's smoothie challenge.sql
@@ -0,0 +1,328 @@
+select * from @trails_geojson (file_format => ff_json);
+
+select * from @trails_parquet (file_format => ff_parquet);
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT
+'DLKW05' as step
+ ,( select sum(tally)
+   from
+     (select count(*) as tally
+      from mels_smoothie_challenge_db.information_schema.stages 
+      union all
+      select count(*) as tally
+      from mels_smoothie_challenge_db.information_schema.file_formats)) as actual
+ ,4 as expected
+ ,'Camila\'s Trail Data is Ready to Query' as description
+ ); 
+
+
+select 
+    $1:sequence_1 as sequence_1,
+    $1:trail_name::varchar as trail_name,
+    $1:latitude as latitude,
+    $1:longitude as longitude,
+    $1:sequence_2 as sequence_2,
+    $1:elevation as elevation
+from @trails_parquet
+(file_format => ff_parquet) order by sequence_1;
+
+--Nicely formatted trail data
+select 
+ $1:sequence_1 as point_id,
+ $1:trail_name::varchar as trail_name,
+ $1:latitude::number(11,8) as lng, --remember we did a gut check on this data
+ $1:longitude::number(11,8) as lat
+from @trails_parquet
+(file_format => ff_parquet)
+order by point_id;
+
+create or replace view CHERRY_CREEK_TRAIL as select 
+ $1:sequence_1 as point_id,
+ $1:trail_name::varchar as trail_name,
+ $1:latitude::number(11,8) as lng, --remember we did a gut check on this data
+ $1:longitude::number(11,8) as lat
+from @trails_parquet
+(file_format => ff_parquet)
+order by point_id;
+
+--Using concatenate to prepare the data for plotting on a map
+select top 100 
+ lng||' '||lat as coord_pair
+,'POINT('||coord_pair||')' as trail_point
+from cherry_creek_trail;
+
+--To add a column, we have to replace the entire view
+--changes to the original are shown in red
+create or replace view cherry_creek_trail as
+select 
+ $1:sequence_1 as point_id,
+ $1:trail_name::varchar as trail_name,
+ $1:latitude::number(11,8) as lng,
+ $1:longitude::number(11,8) as lat,
+ lng||' '||lat as coord_pair
+from @trails_parquet
+(file_format => ff_parquet)
+order by point_id;
+
+select 
+'LINESTRING('||
+listagg(coord_pair, ',') 
+within group (order by point_id)
+||')' as my_linestring
+from cherry_creek_trail
+where point_id <= 10
+group by trail_name;
+
+select
+$1:features[0]:properties:Name::string as feature_name
+,$1:features[0]:geometry:coordinates::string as feature_coordinates
+,$1:features[0]:geometry::string as geometry
+,$1:features[0]:properties::string as feature_properties
+,$1:crs:properties:name::string as specs
+,$1 as whole_object
+from @trails_geojson (file_format => ff_json);
+
+create or replace view DENVER_AREA_TRAILS as select
+$1:features[0]:properties:Name::string as feature_name
+,$1:features[0]:geometry:coordinates::string as feature_coordinates
+,$1:features[0]:geometry::string as geometry
+,$1:features[0]:properties::string as feature_properties
+,$1:crs:properties:name::string as specs
+,$1 as whole_object
+from @trails_geojson (file_format => ff_json);
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT
+'DLKW06' as step
+ ,( select count(*) as tally
+      from mels_smoothie_challenge_db.information_schema.views 
+      where table_name in ('CHERRY_CREEK_TRAIL','DENVER_AREA_TRAILS')) as actual
+ ,2 as expected
+ ,'Mel\'s views on the geospatial data from Camila' as description
+ ); 
+
+ --Remember this code? 
+select 
+'LINESTRING('||
+listagg(coord_pair, ',') 
+within group (order by point_id)
+||')' as my_linestring
+,st_length(TO_GEOGRAPHY(my_linestring)) as length_of_trail --this line is new! but it won't work!
+from cherry_creek_trail
+group by trail_name;
+
+select 
+'LINESTRING('||
+listagg(FEATURE_COORDINATES, ',') 
+within group (order by FEATURE_COORDINATES)
+||')' as my_linestring
+,st_length(TO_GEOGRAPHY(my_linestring)) as length_of_trail --this line is new! but it won't work!
+from DENVER_AREA_TRAILS
+group by FEATURE_NAME;
+
+
+select feature_name
+, st_length(to_geography(GEOMETRY)) as wo_length
+, st_length(to_geography(GEOMETRY)) as geom_length
+from denver_area_trails;
+
+
+create or replace view denver_area_trails(
+    feature_name,
+    feature_coordinates,
+    geometry,
+    trail_length,
+    feature_properties,
+    specs,
+    whole_object
+) as 
+select 
+$1:features[0]:properties:Name::string as feature_name
+,$1:features[0]:geometry:coordinates::string as feature_coordinates
+,$1:features[0]:geometry::string as geometry
+,st_length(to_geography(GEOMETRY)) as trail_legth
+,$1:features[0]:properties::string as feature_properties
+,$1:crs:properties:name::string as specs
+,$1 as whole_object
+from @trails_geojson (file_format => ff_json);
+
+select * from denver_area_trails;
+
+select * from cherry_creek_trail;
+
+--Create a view that will have similar columns to DENVER_AREA_TRAILS 
+--Even though this data started out as Parquet, and we're joining it with geoJSON data
+--So let's make it look like geoJSON instead.
+create or replace view DENVER_AREA_TRAILS_2 as
+select 
+trail_name as feature_name
+,'{"coordinates":['||listagg('['||lng||','||lat||']',',') within group (order by point_id)||'],"type":"LineString"}' as geometry
+,st_length(to_geography(geometry))  as trail_length
+from cherry_creek_trail
+group by trail_name;
+
+--Create a view that will have similar columns to DENVER_AREA_TRAILS 
+select feature_name, geometry, trail_length
+from DENVER_AREA_TRAILS
+union all
+select feature_name, geometry, trail_length
+from DENVER_AREA_TRAILS_2;
+
+--Add more GeoSpatial Calculations to get more GeoSpecial Information! 
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS
+union all
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS_2;
+
+create or replace view trails_and_boundaries as 
+--Add more GeoSpatial Calculations to get more GeoSpecial Information! 
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS
+union all
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS_2;
+
+select * from trails_and_boundaries;
+
+select 'POLYGON(('|| 
+    min(min_eastwest)||' '||max(max_northsouth)||','|| 
+    max(max_eastwest)||' '||max(max_northsouth)||','|| 
+    max(max_eastwest)||' '||min(min_northsouth)||','|| 
+    min(min_eastwest)||' '||min(min_northsouth)||'))' AS my_polygon
+from trails_and_boundaries;
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+  'DLKW07' as step
+   ,( select round(max(max_northsouth))
+      from MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.TRAILS_AND_BOUNDARIES)
+      as actual
+ ,40 as expected
+ ,'Trails Northern Extent' as description
+ );
+
+
+
+
+
+ CREATE OR REPLACE FUNCTION distance_to_mc(loc_lng number(38,32),loc_lat number(38,32))
+  RETURNS FLOAT
+  AS
+  $$
+   st_distance(
+        st_makepoint(-104.9730, 39.7647)
+        ,st_makepoint(loc_lng,loc_lat)
+        )
+  $$
+  ;
+
+  --Tivoli Center into the variables 
+set tc_lng='-105.00532059763648'; 
+set tc_lat='39.74548137398218';
+
+select distance_to_mc($tc_lng,$tc_lat);
+
+SELECT
+ name
+ ,cuisine
+ , ST_DISTANCE(
+    st_makepoint('-104.97300245114094','39.76471253574085')
+    , coordinates
+  ) AS distance_to_melanies
+ ,*
+FROM  competition
+ORDER by distance_to_melanies;
+
+CREATE OR REPLACE FUNCTION distance_to_mc(lng_and_lat GEOGRAPHY)
+  RETURNS FLOAT
+  AS
+  $$
+   st_distance(
+        st_makepoint('-104.97300245114094','39.76471253574085')
+        ,lng_and_lat
+        )
+  $$
+  ;
+  
+
+  SELECT
+ name
+ ,cuisine
+ ,distance_to_mc(coordinates) AS distance_to_melanies
+ ,*
+FROM  competition
+ORDER by distance_to_melanies;
+
+
+-- Tattered Cover Bookstore McGregor Square
+set tcb_lng='-104.9956203'; 
+set tcb_lat='39.754874';
+
+--this will run the first version of the UDF
+select distance_to_mc($tcb_lng,$tcb_lat);
+
+--this will run the second version of the UDF, bc it converts the coords 
+--to a geography object before passing them into the function
+select distance_to_mc(st_makepoint($tcb_lng,$tcb_lat));
+
+
+
+create or replace external table T_CHERRY_CREEK_TRAIL(
+	my_filename varchar(100) as (metadata$filename::varchar(100))
+) 
+location= @external_aws_dlkw
+auto_refresh = true
+file_format = (type = parquet);
+
+select * from T_CHERRY_CREEK_TRAIL;
+
+
+set table_name = 'CCT_'||current_account();
+
+create iceberg table identifier($table_name) (
+    point_id number(10,0)
+    , trail_name string
+    , coord_pair string
+    , distance_to_melanies decimal(20,10)
+    , user_name string
+)
+  BASE_LOCATION = $table_name
+  AS SELECT top 100
+    point_id
+    , trail_name
+    , coord_pair
+    , distance_to_melanies
+    , current_user()
+  FROM MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.SMV_CHERRY_CREEK_TRAIL;
